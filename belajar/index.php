4bf0259af53c919fe5530314f3a4a6ff6d5037cc
kapuas <mqor@s.bloq.ro>
2017-05-02 11:19:12 +0700

    Belajar include dan require once

---
+++ belajar/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Sisfo Untan</title>
+</head>
+<body>
+	<h1>Sistem Informasi</h1>
+<?php 
+include_once 'tabel.php';
+require_once 'satu.php';
+
+?>
+
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo vitae, consequatur nulla quam doloremque quasi hic, fugiat unde voluptate tenetur, quod ratione nisi eveniet consectetur fugit velit iure esse molestiae.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt eligendi, ullam eos neque voluptatem, ipsum odio doloremque officiis dolore possimus veniam eius ducimus? Maxime laudantium corporis eos quod quia veniam.</p>
+	<?php 
+include_once 'tabel.php';
+	 ?>
+</body>
+</html>
---
+++ belajar/tabel.php
@@ -0,0 +1,12 @@
+<table border="1">
+	<tr>
+		<td>No</td>
+		<td>Nim</td>
+		<td>Nama</td>
+	</tr>
+	<tr>
+		<td>1</td>
+		<td>X010101010</td>
+		<td>Dian Prawira</td>
+	</tr>
+</table>
